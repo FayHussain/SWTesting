d86c98a23d4473539d448179ff5c3d2c3af50848
Lshalawi <117104297+Lshalawi@users.noreply.github.com>
2022-11-02 20:08:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function sum(a,b){
 module.exports.sum = sum
 let x = 1;
 console.log(x);
+
+<p id="mooncake"></p>
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("mooncake").innerHTML = myFunction(4, 3);
