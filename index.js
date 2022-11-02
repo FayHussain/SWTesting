a3c2cb67a60c1b1041bcefd9c019286def7b9db3
Lshalawi <117104297+Lshalawi@users.noreply.github.com>
2022-11-02 20:00:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function sum(a,b){
 module.exports.sum = sum
 let x = 1;
 console.log(x);
+
+<script>
+var x = myFunction(4, 3);
+document.getElementById("demo").innerHTML = x;
+
+function myFunction(a, b) {
+  return a * b;
+}
+</script>
